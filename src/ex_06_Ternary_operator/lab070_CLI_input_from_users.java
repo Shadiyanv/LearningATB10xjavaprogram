72de23f3aad5966a05c7e2a4cba46b3c6f903079
Shadiya NV <shadiyahameed1999@gmail.com>
2025-04-16 09:53:08 +0530

    16 Apr

--- src/ex_06_Ternary_operator/lab070_CLI_input_from_users.java
+++ src/ex_06_Ternary_operator/lab070_CLI_input_from_users.java
@@ -3,6 +3,7 @@ package ex_06_Ternary_operator;
 public class lab070_CLI_input_from_users {
     public static void main(String[] args) {
         //take input
+        //take input--->run button-->edit configuration-->give input
         String age_String=args[0];
         int age=Integer.parseInt(age_String);
         System.out.println(age);
